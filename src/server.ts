5ed8f2322c36e7258021c77942223d30c308794a
victor.souza <victor.sozua@koin.com.br>
2025-05-12 02:25:43 -0300

    adicionando rota de finance

--- src/server.ts
+++ src/server.ts
@@ -7,6 +7,7 @@ import { authRoutes } from "./modules/auth/auth.routes";
 import authenticatePlugin from "plugins/authenticate";
 import jwtPlugin from "plugins/jwt";
 import { studentRoutes } from "modules/student/student.routes";
+import { financeRoutes } from "modules/finance/finance.routes";
 
 const app = Fastify({
   logger: true,
@@ -21,6 +22,7 @@ app.register(jwtPlugin);
 app.register(authenticatePlugin);
 app.register(authRoutes, { prefix: "api/" });
 app.register(studentRoutes, { prefix: "api/" });
+app.register(financeRoutes, { prefix: "api/" });
 app.get("/", async (request, reply) => {
   return { message: "API is running ", db: app.config.DATABASE_URL };
 });
