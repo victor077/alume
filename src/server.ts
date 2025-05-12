78955c0eb1e0d8bb9bc4175e7ef063e6b00986f5
victor.souza <victor.sozua@koin.com.br>
2025-05-11 23:36:44 -0300

    adicioando plugins para rodar na app

--- src/server.ts
+++ src/server.ts
@@ -3,6 +3,10 @@ import "dotenv/config";
 import configPlugin from "./plugins/config";
 import prismaPlugin from "./plugins/prisma";
 import swaggerPlugin from "./plugins/swagger";
+import { authRoutes } from "./modules/auth/auth.routes";
+import authenticatePlugin from "plugins/authenticate";
+import jwtPlugin from "plugins/jwt";
+import { studentRoutes } from "modules/student/student.routes";
 
 const app = Fastify({
   logger: true,
@@ -13,7 +17,10 @@ app.register(swaggerPlugin, {
   dependencies: ["config"],
 });
 app.register(prismaPlugin, { dependencies: ["config"] });
-
+app.register(jwtPlugin);
+app.register(authenticatePlugin);
+app.register(authRoutes, { prefix: "api/" });
+app.register(studentRoutes, { prefix: "api/" });
 app.get("/", async (request, reply) => {
   return { message: "API is running ", db: app.config.DATABASE_URL };
 });
